3d797f02fbbb9cbea8eeb6de9fcbbe6aa93b2d23
shannon-codes <shannonlim8@gmail.com>
2020-10-04 11:54:03 -0400

    last commit

--- frontend/frontendRBC/src/app/app.module.ts
+++ frontend/frontendRBC/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeOpportunitiesComponent } from './employee-opportunities/employee-opportunities.component';
-import {ReactiveFormsModule} from '@angular/forms'; 
-import {FormsModule} from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms'; 
 import { MatCardModule } from '@angular/material/card';
 import { EmployeeProfileComponent } from './employee-profile/employee-profile.component';
 import { CustomerAdvisorsComponent } from './customer-advisors/customer-advisors.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+
+
 @NgModule({
   declarations: [
     AppComponent,

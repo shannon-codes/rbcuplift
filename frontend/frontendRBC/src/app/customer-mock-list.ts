fc503e98dcb09bf2affe2b12bb8e280df05a0bb1
shannon-codes <shannonlim8@gmail.com>
2020-10-29 12:32:40 -0400

    update assets path

--- frontend/frontendRBC/src/app/customer-mock-list.ts
+++ frontend/frontendRBC/src/app/customer-mock-list.ts
@@ -1,16 +1,16 @@
 export const tempList=[
-  { id: 1, photo: '../../assets/logo1.png', name: 'Accounting', title: 'Scoops Ice Cream Parlour', status: true },
-  { id: 2, photo: '../../assets/logo2.png', name: 'Website Feedback', title: 'NuWorld Botanicals', status: true },
-  { id: 3, photo: '../../assets/logo3.jpg', name: 'Marketing', title: 'Shop Mom and Pop', status: true },
-  { id: 1, photo: '../../assets/logo1.png', name: 'Accounting', title: 'Scoops Ice Cream Parlour', status: true },
-  { id: 2, photo: '../../assets/logo2.png', name: 'Website Feedback', title: 'NuWorld Botanicals', status: true },
-  { id: 3, photo: '../../assets/logo3.jpg', name: 'Marketing', title: 'Shop Mom and Pop', status: true },
-  { id: 1, photo: '../../assets/logo1.png', name: 'Accounting', title: 'Scoops Ice Cream Parlour', status: true },
-  { id: 2, photo: '../../assets/logo2.png', name: 'Website Feedback', title: 'NuWorld Botanicals', status: true },
-  { id: 3, photo: '../../assets/logo3.jpg', name: 'Marketing', title: 'Shop Mom and Pop', status: true },
-  { id: 1, photo: '../../assets/logo1.png', name: 'Accounting', title: 'Scoops Ice Cream Parlour', status: true },
-  { id: 2, photo: '../../assets/logo2.png', name: 'Website Feedback', title: 'NuWorld Botanicals', status: true },
-  { id: 3, photo: '../../assets/logo3.jpg', name: 'Marketing', title: 'Shop Mom and Pop', status: true },
+  { id: 1, photo: 'assets/logo1.png', name: 'Accounting', title: 'Scoops Ice Cream Parlour', status: true },
+  { id: 2, photo: 'assets/logo2.png', name: 'Website Feedback', title: 'NuWorld Botanicals', status: true },
+  { id: 3, photo: 'assets/logo3.jpg', name: 'Marketing', title: 'Shop Mom and Pop', status: true },
+  { id: 1, photo: 'assets/logo1.png', name: 'Accounting', title: 'Scoops Ice Cream Parlour', status: true },
+  { id: 2, photo: 'assets/logo2.png', name: 'Website Feedback', title: 'NuWorld Botanicals', status: true },
+  { id: 3, photo: 'assets/logo3.jpg', name: 'Marketing', title: 'Shop Mom and Pop', status: true },
+  { id: 1, photo: 'assets/logo1.png', name: 'Accounting', title: 'Scoops Ice Cream Parlour', status: true },
+  { id: 2, photo: 'assets/logo2.png', name: 'Website Feedback', title: 'NuWorld Botanicals', status: true },
+  { id: 3, photo: 'assets/logo3.jpg', name: 'Marketing', title: 'Shop Mom and Pop', status: true },
+  { id: 1, photo: 'assets/logo1.png', name: 'Accounting', title: 'Scoops Ice Cream Parlour', status: true },
+  { id: 2, photo: 'assets/logo2.png', name: 'Website Feedback', title: 'NuWorld Botanicals', status: true },
+  { id: 3, photo: 'assets/logo3.jpg', name: 'Marketing', title: 'Shop Mom and Pop', status: true },
 
 ];
 
